f349d84363fc43d163cbbf4278408fa379e97a34
julien wist <julien wist@correounivalle.edu.co>
2023-12-22 09:32:11 +0100

    improving readLipo

--- R/readLipo.r
+++ R/readLipo.r
@@ -1,14 +1,14 @@
 #' extract lipoprotein  quantification information from a bruker xml
 #'
-#' @param path - the path to the expName folder
+#' @param file - the path to the expName folder
 #' @return the values
 #'
 #' @export
 #' @importFrom xml2 read_xml xml_attr xml_find_all
-readLipo <- function(path){
+readLipo <- function(file){
   path <- file.path(path, "pdata", "1", "lipo_results.xml")
-  if (file.exists(path)) {
-    xml <- read_xml(path, options = "NOBLANKS")
+  if (file.exists(file)) {
+    xml <- read_xml(file, options = "NOBLANKS")
     comment <- xml_attr(xml_find_all(xml, ".//PARAMETER"), "comment")
     id <- xml_attr(xml_find_all(xml, ".//PARAMETER"), "name")
     type <- xml_attr(xml_find_all(xml, ".//PARAMETER"), "type")
@@ -35,7 +35,7 @@ readLipo <- function(path){
     fi <- duplicated(res$id)
     return(res[!fi,])
   } else {
-    cat(crayon::yellow("fusion::readLipo >>", path, "not found\n"))
+    cat(crayon::yellow("fusion::readLipo >>", file, "not found\n"))
   }
 }
 
@@ -43,79 +43,5 @@ readLipo <- function(path){
 # makeLipoReport(lip, options = list(fold = FALSE, dotColor = "blue", dotPch = 21))
 # addValues(lip, options = list(dotColor = "red", dotPch = 22))
 
-#' get names and description of lipoproteins IVDr parameters
-#' @return a data.frame with information
-#' @importFrom utils data
-#' @export
-getLipoTable <- function() {
-  lipo <- get0("lipo", envir = asNamespace("nmr.parser"))
-  lipo$range <- paste0(lipo$refMin, " - ", lipo$refMax,
-                       " (", lipo$refUnit, ")")
-  names(lipo) <-c("Fraction",
-                  "Compound",
-                  "Abbreviation",
-                  "ID",
-                  "Type",
-                  "Value",
-                  "Unit",
-                  "Max Value (ref.)",
-                  "Min Value (ref.)",
-                  "Reference Unit",
-                  "Reference Range [Unit]")
-  # correcting typo in xml
-  lipo$Compound[9] <- "Apo-B100 / Apo-A1"
-  rownames(lipo) <- c(1:nrow(lipo))
 
-  # cleaning column text
-  lipo$Compound <- gsub(" ", "", lipo$Compound)
-  lipo$Abbreviation <- gsub(" ", "", lipo$Abbreviation)
-
-  # creating label for publication
-  tag <- paste0(lipo$Compound, ", ", lipo$Abbreviation)
-  tag[1] <- "Triglycerides, total"
-  tag[2] <- "Cholesterol, total"
-  tag[3] <- "Cholesterol, LDL"
-  tag[4] <- "Cholesterol, HDL"
-  tag[5] <- "Apo-A1, total"
-  tag[6] <- "Apo-A2, total"
-  tag[7] <- "Apo-B100, total"
-
-  tag[8] <- "LDL-Chol/HDL-Chol"
-  tag[9] <- "Apo-B100/Apo-A1"
-  tag[10] <- "Apo-B100, particle number"
-  tag[11] <- "VLDL, particle number"
-  tag[12] <- "IDL, particle number"
-  tag[13] <- "LDL, particle number"
-  tag[14] <- "LD1, particle number"
-  tag[15] <- "LD2, particle number"
-  tag[16] <- "LD3, particle number"
-  tag[17] <- "LD4, particle number"
-  tag[18] <- "LD5, particle number"
-  tag[19] <- "LD6, particle number"
-
-  lipo$tag <- tag
-
-  return(lipo[,c(1,2,4,11, 12)])
-}
-
-#' get lipo for rolodex
-#' @param path - path to the data
-#' @return a triad
-#' @importFrom xml2 read_xml xml_attr xml_find_all xml_find_first
-#' @export
-roldx_getLipo <- function(path){
-  if (file.exists(path)) {
-    xml <- read_xml(path, options = "NOBLANKS")
-    version <- xml_attr(xml_find_first(xml, ".//QUANTIFICATION"), "version")
-    version <- strsplit(version, " ")[[1]][1]
-    id <- xml_attr(xml_find_all(xml, ".//PARAMETER"), "name")
-    value <- xml_attr(xml_find_all(xml, ".//VALUE"), "value")
-
-    # there is one duplicated id
-    fi <- duplicated(id)
-    return(list(id = id[!fi], value = as.numeric(value[!fi]), version = version))
-  } else {
-    cat(crayon::yellow("fusion::readLipo >>", path, "not found\n"))
-  }
-}
 
--- tests/testthat/test-roldx_readLipo.R
+++ tests/testthat/test-roldx_readLipo.R
@@ -1,12 +1,12 @@
 test_that("reading inexistent file", {
   path <- system.file("..", "tests", "testthat", "lipo_results", package = "nmr.parser")
-  lipo <- roldx_getLipo(path)
+  lipo <- roldx_readLipo(path)
   expect_equal(lipo, NULL)
 })
 
 test_that("reading file", {
   path <- system.file("..", "tests", "testthat", "lipo_results.xml", package = "nmr.parser")
-  lipo <- roldx_getLipo(path)
+  lipo <- roldx_readLipo(path)
   expect_equal(lipo$id[1], "TPTG")
   expect_equal(lipo$value[1], 69.49)
   expect_equal(length(lipo$value), 112)
