e9c47270f514bd0c92a85cdab75619737de40609
julien wist <julien wist@correounivalle.edu.co>
2024-01-26 00:02:26 +0800

    fixing parameters for comet

--- R/readParams.r
+++ R/readParams.r
@@ -16,13 +16,21 @@ readParams <- function(file) {
       return(NULL)
     }
 
+
     content <- list()
     counter <- 1 # line counter
+    incr <- 1
     while (counter < length(txt)) {
+
+
       line <- txt[[counter]]
+
+
       if (grepl("^##END=", line)) {
         break
       }
+
+
       path <- strsplit(file, "/")[[1]]
       # get titles
       if (grepl("^##[A-Z]", line)) {
@@ -42,73 +50,94 @@ readParams <- function(file) {
         cleanValue <- gsub("\\t", " ", value)
         cleanValue <- gsub("\\$\\$", "", cleanValue)
         cleanValue <- gsub("\\s+", " ", cleanValue)
-        content[[counter]] <- list(path = path[length(path)],
-                                     name = gsub("##", "", param[1]),
-                                     value = cleanValue)
+        content[[incr]] <- list(path = path[length(path)],
+                                name = gsub("##", "", param[1]),
+                                value = cleanValue)
       } else
 
-      # get audit info
-      if (grepl("^\\$\\$\\s", line)) {
-        date <- time <- timezone <- instrument <- NULL
-        param <- gsub("\\$\\$\\s", "", line)
-        param <- gsub("\\s+", " ", param)
-
-        if (grepl("^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]", param)) {
-          date <- strsplit(param, " ")[[1]][1]
-          time <- strsplit(param, " ")[[1]][2]
-          timezone <- strsplit(param, " ")[[1]][3]
-          instrument <- cleanNames(strsplit(param, " ")[[1]][4])
-        } else if (grepl("^[A-Z][a-z][a-z]\\s[A-Z][a-z][a-z]\\s[0-9]", param)) {
+        # get audit info
+        if (grepl("^\\$\\$\\s", line)) {
+          date <- time <- timezone <- instrument <- NULL
+          param <- gsub("\\$\\$\\s", "", line)
           param <- gsub("\\s+", " ", param)
-          date <- paste0(strsplit(param, " ")[[1]][c(1:3,5)], collapse = " ")
-          time <- strsplit(param, " ")[[1]][4]
-          timezone <- paste0(strsplit(param, " ")[[1]][c(6,7)], collapse = " ")
-          instrument <- cleanNames(strsplit(param, " ")[[1]][8])
-        }
 
-        if (!is.null(date) & !is.null(time) & !is.null(timezone) & !is.null(instrument)) {
-          content[[counter]] <- list(path = path[length(path)],
-                                       name = "instrumentDate",
-                                       value = date)
-          content[[counter]] <- list(path = path[length(path)],
-                                       name = "instrumentTime",
-                                       value = time)
-          content[[counter]] <- list(path = path[length(path)],
-                                       name = "instrumentTimeZone",
-                                       value = timezone)
-          content[[counter]] <- list(path = path[length(path)],
-                                       name = "instrument",
-                                       value = instrument)
-        }
+          # XwinNMR
+          if (grepl("^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]", param)) {
 
-      } else
+            date <- strsplit(param, " ")[[1]][1]
+            time <- strsplit(param, " ")[[1]][2]
+            timezone <- strsplit(param, " ")[[1]][3]
+            instrument <- cleanNames(strsplit(param, " ")[[1]][4])
 
-      # get params
-      if (grepl("^##\\$", line)) {
-        param <- strsplit(line, "= ")[[1]]
-        value <- param[2]
-        # look for vectors
-        if (grepl("\\([0-9]+..[0-9]+\\)", value)) {
-          counter <- counter + 1
-          vect <- txt[[counter]]
-          value <- strsplit(vect, " ")
-          for (i in 1:length(value[[1]])) {
-            content[[counter]] <- list(path = path[length(path)],
-                                         name = paste0(gsub("##\\$", "", param[1]), "_", i - 1),
-                                         value = value[[1]][i])
+            # Topspin
+          } else if (grepl("^[A-Z][a-z][a-z]\\s[A-Z][a-z][a-z]\\s[0-9]", param)) {
+
+            param <- gsub("\\s+", " ", param)
+            date <- paste0(strsplit(param, " ")[[1]][c(1:3,5)], collapse = " ")
+            time <- strsplit(param, " ")[[1]][4]
+            timezone <- paste0(strsplit(param, " ")[[1]][c(6,7)], collapse = " ")
+            instrument <- cleanNames(strsplit(param, " ")[[1]][8])
+
           }
-        } else {
-          cleanValue <- gsub("[<->]", "", value)
-          content[[counter]] <- list(path = path[length(path)],
-                                       name = gsub("##\\$", "", param[1]),
-                                       value = cleanValue)
-        }
-      }
+
+          if (!is.null(date) & !is.null(time) & !is.null(timezone) & !is.null(instrument)) {
+            content[[incr]] <- list(path = path[length(path)],
+                                    name = "instrumentDate",
+                                    value = date)
+            incr <- incr + 1
+            content[[incr]] <- list(path = path[length(path)],
+                                    name = "instrumentTime",
+                                    value = time)
+            incr <- incr + 1
+            content[[incr]] <- list(path = path[length(path)],
+                                    name = "instrumentTimeZone",
+                                    value = timezone)
+            incr <- incr + 1
+            content[[incr]] <- list(path = path[length(path)],
+                                    name = "instrument",
+                                    value = instrument)
+          }
+
+        } else
+
+          # get params
+          if (grepl("^##\\$", line)) {
+            param <- strsplit(line, "= ")[[1]]
+            value <- param[2]
+
+
+            # looking for vectors
+            if (grepl("\\([0-9]+..[0-9]+\\)", value)) {
+              counter <- counter + 1
+              vect <- txt[[counter]]
+              value <- strsplit(vect, " ")
+              for (i in 1:length(value[[1]])) {
+                content[[incr]] <- list(path = path[length(path)],
+                                        name = paste0(gsub("##\\$", "", param[1]), "_", i - 1),
+                                        value = value[[1]][i])
+
+                incr <- incr + 1
+
+              }
+            } else {
+              cleanValue <- gsub("[<->]", "", value)
+              content[[incr]] <- list(path = path[length(path)],
+                                      name = gsub("##\\$", "", param[1]),
+                                      value = cleanValue)
+            }
+          }
+
+
       counter <- counter + 1
+      incr <- incr + 1
     }
+
+
+
     ret <- data.frame(do.call(rbind, content))
     fi <- sapply(ret$value, function(x) identical(x, character(0)))
     ret$value[fi] <- NA
+
 
     return(data.frame(path = unlist(ret$path),
                       name = unlist(ret$name),
