fc01e020beb22f9f1635dec3d2a9f7806fe32881
julien wist <julien wist@correounivalle.edu.co>
2024-09-06 11:32:26 +0800

    tyding typos

--- R/brxsm_uri-data.R
+++ R/brxsm_uri-data.R
@@ -1,11 +1,11 @@
 #' Small molecules from IVDr method
 #'
-#' A dataset containing 26 metabolites from urine
+#' A dataset containing 150 metabolites from urine
 #'
 #'
 #' @docType data
 #' @keywords datasets
 #' @name brxsm_uri
 #' @usage data(brxsm_uri)
-#' @format A data frame with 26 urine metabolites concentration ranges
+#' @format A data frame with 150 urine metabolites concentration ranges
 data("brxsm_uri", envir = asNamespace("nmr.parser"))
