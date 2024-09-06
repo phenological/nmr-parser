e5d172f16f52babc97a754b0215f83275e1f7567
julien wist <julien wist@correounivalle.edu.co>
2024-09-06 16:49:01 +0800

    release 0.2.5

--- tests/testthat/test-readLipo.R
+++ tests/testthat/test-readLipo.R
@@ -5,10 +5,12 @@ test_that("reading inexistent file", {
 
 test_that("reading file", {
   lipo <- readLipo(system.file("..", "tests", "testthat", "lipo_results.xml", package = "nmr.parser"))
+  names(lipo$data)
   expect_equal(lipo$data$id[1], "TPTG")
   expect_equal(lipo$data$fraction[1], "Main Parameters")
   expect_equal(lipo$data$value[1], 69.49)
   expect_equal(length(lipo$data$value), 112)
+  expect_equal(ncol(lipo$data), 10)
   expect_equal(lipo$version, "PL-5009-01/001")
 })
 
@@ -16,10 +18,12 @@ test_that("reading file", {
 test_that("reading file", {
   lipo <- readLipo(system.file("..", "tests", "testthat", "lipo_results.xml", package = "nmr.parser"))
   lipo <- extend_lipo(lipo)
+  names(lipo$data)
 
   expect_equal(lipo$data$id[1], "TPTG")
   expect_equal(lipo$data$fraction[1], "Main Parameters")
   expect_equal(lipo$data$value[1], 69.49)
   expect_equal(length(lipo$data$value), 316)
+  expect_equal(ncol(lipo$data), 11)
   expect_equal(lipo$version, "PL-5009-01/001")
 })
