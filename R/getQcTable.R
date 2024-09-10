fd5fde409dd92d606dc7f2cd5af2cccbfc6ca848
julien wist <julien wist@correounivalle.edu.co>
2024-09-10 17:24:55 +0800

    add getQcTable

---
+++ R/getQcTable.R
@@ -0,0 +1,26 @@
+
+
+getQcTable <- function() {
+
+  qc <- readQc(system.file("..",
+                           "tests",
+                           "testthat",
+                           "plasma_qc_report.xml",
+                           package = "nmr.parser"))
+
+  tbl <- data.frame(testName = qc$tests$name,
+                      testType = qc$tests$type,
+                      testUnit = qc$tests$unit,
+                      testRefMax = qc$tests$refMax,
+                      testRefMin = qc$tests$refMin,
+                      testDescription = qc$tests$comment)
+
+  tbl2 <- data.frame(testName = qc$infos$name,
+                     testType = NA,
+                     testUnit = NA,
+                     testRefMax = NA,
+                     testRefMin = NA,
+                     testDescription = qc$infos$value)
+
+  return(rbind(tbl2, tbl1))
+}
